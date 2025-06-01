1aa63f2093c1ff8b13727bcc698f142f11356cca
sahe <jackitehm@gmail.com>
2025-06-01 21:34:24 +0300

    Setting up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage = () => {
-  return <div className="p-8 text-center">Posts Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8 text-center">
+        <h1 className="text-3xl font-semibold">Posts Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage = () => {
-  return <div className="p-8 text-center">Users Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8 text-center">
+        <h1 className="text-3xl font-semibold">Users Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default UsersPage;
